c6241923fd0071c9c7cd4d322727e39df0f3cb20
ArchLeaders <archparliament@gmail.com>
2023-05-14 21:27:11 -0700

    Update txtg header

--- src/include/txtg/txtg.h
+++ src/include/txtg/txtg.h
@@ -10,6 +10,39 @@
 
 namespace oepd {
 
-namespace txtg {}  // namespace txtg
+namespace txtg {
+
+struct Header {
+  u16 header_size;
+  u16 version;
+  std::array<char, 4> magic;
+  u16 width;
+  u16 height;
+  u16 texture_count;
+  u8 mip_map_count;
+  u8 unknown_1;  // Always 0x02?
+  std::array<char, 0x0C> unknown_data_block;
+  std::array<char, 0x20> checksum;
+  u16 format;
+  u16 unknown_2;  // Always 0x0300?
+  f32 unknown_3;  // Float Value
+  u32 unknown_4;  // Second byte is typically 0xF7
+  u16 unknown_6;  // Always 0x0200?
+  u16 unknown_7;  // Always 0x0200?
+  u16 unknown_8;  // First bytes is always(?) 0x2, second byte changes (3, 5, 7...)
+  u16 unknown_9;  // Always 0x01?
+};
+
+struct TextureEntry {
+  u16 texture_index;
+  u8 mip_index;
+  u8 unk1;  // always 1?
+};
+
+class TXTG {
+  TXTG(tcb::span<const u8> data);
+};
+
+}  // namespace txtg
 
 }  // namespace oepd
